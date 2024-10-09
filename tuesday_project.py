efd1e88f958d4eb747dc33406efd5a7e7b7fec0f
Jorge Junior <jorge.jv321@gmail.com>
2024-10-08 23:26:13 -0300

    arquivos adicionados via upload

---
+++ tuesday_project.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+st.set_page_config(
+    page_title="Meu Aplicativo Streamlit",
+    page_icon=":rocket:",  
+    layout="wide")
+
+ 
+#lendo o arquivo csv
+
+df = pd.read_excel('C://Repos//Tuesday Status//TuesdayStatus.xlsx')
+
+
+# Barra lateral no Streamlit
+
+st.header('V1 | Painel de Controle - RCAs')
+
+st.sidebar.markdown('# Bem vindo (a)!')
+st.sidebar.markdown("---")
+st.sidebar.markdown('## Painel estruturado com linguagem Python')
+st.sidebar.markdown("---")
+st.sidebar.markdown("## Powered by Governance - TORRA")
+
+image = Image.open('C://Repos//Tuesday Status//torra-1024.png')
+st.sidebar.image( image)
+
+tab1, = st.tabs(['Visão geral'])
+
+with tab1: 
+    with st.container():
+        st.title(' RCAs mapeados' )
+        st.dataframe(df)
