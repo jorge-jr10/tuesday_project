3bd28fbc92c224534c930b8d777f566bb8aea9a6
Jorge Junior <jorge.jv321@gmail.com>
2024-10-09 00:25:11 -0300

    Update tuesday_project.py

--- tuesday_project.py
+++ tuesday_project.py
@@ -32,6 +32,6 @@ tab1, = st.tabs(['Visão geral'])
 with tab1:
     with st.container():
         st.title(' RCAs mapeados' )
-        st.dataframe(df)
+
 
         st.dataframe(df, use_container_width=True)
