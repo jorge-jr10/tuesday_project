c11e554d9ab02758f10d2bef4ccc523ee70a5052
Jorge Junior <jorge.jv321@gmail.com>
2024-10-22 17:12:03 -0300

    Update tuesday_project.py

--- tuesday_project.py
+++ tuesday_project.py
@@ -10,46 +10,31 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="expanded")
 
-# Definir senha correta
-senha_correta = "191001"
 
-# Criar campo para o usuário digitar a senha
-senha = st.text_input("Digite a senha:", type="password")
-
-# Verificar se a senha está correta
-if senha == senha_correta:
-    st.success("Senha correta! Bem-vindo ao app.")
-       
-       # Coloque aqui o conteúdo do seu app
-       st.write("Aqui está o conteúdo protegido do app.")
-
-       #lendo o arquivo csv
-       df = pd.read_excel('TuesdayStatus2 - 22.10.24.xlsx')
+#lendo o arquivo csv
+df = pd.read_excel('TuesdayStatus2 - 22.10.24.xlsx')
            
-       # Barra lateral no Streamlit
-       st.header("V1 | Painel de Controle - RCA's")
+# Barra lateral no Streamlit
+st.header("V1 | Painel de Controle - RCA's")
            
-       st.sidebar.markdown('# Bem vindo(a)!')
+st.sidebar.markdown('# Bem vindo(a)!')
            
-       st.sidebar.markdown("---")
+st.sidebar.markdown("---")
            
-       st.sidebar.markdown('## Painel estruturado com linguagem Python')
+st.sidebar.markdown('## Painel estruturado com linguagem Python')
            
-       st.sidebar.markdown("---")
+st.sidebar.markdown("---")
            
-       st.sidebar.markdown("## Powered by Governance - TORRA")
+ st.sidebar.markdown("## Powered by Governance - TORRA")
            
-       #imagem TORRA
-       image = Image.open('torra-1024.png')
-       st.sidebar.image( image )
+#imagem TORRA
+image = Image.open('torra-1024.png')
+st.sidebar.image( image )
            
-       #estrutura da pagina
-       tab1, = st.tabs(['Visão geral'])
+#estrutura da pagina
+tab1, = st.tabs(['Visão geral'])
            
-       with tab1:
-               with st.container():
-                   st.title("RCA's mapeados" )
-                   st.table(df)
-
-else:
-       st.error("Senha incorreta. Tente novamente.")
+with tab1:
+       with st.container():
+       st.title("RCA's mapeados" )
+       st.table(df)
